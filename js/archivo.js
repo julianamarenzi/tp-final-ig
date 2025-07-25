f88f6ca67cb486df943514af6c7eaef80ebc4c10
julianamarenzi <julianamarenzi@gmail.com>
2025-07-25 15:40:33 -0300

    Agrego form, cambio nombre de ruta css

---
+++ js/archivo.js
@@ -0,0 +1,19 @@
+// ------------- Validación de Formulario -------------
+
+(() => {
+    'use strict'
+
+    const forms = document.querySelectorAll('.needs-validation')
+    
+    // Loop over them and prevent submission
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+            event.preventDefault()
+            event.stopPropagation()
+            }
+    
+            form.classList.add('was-validated')
+        }, false)
+    })
+})()
